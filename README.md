de768cd569f94d37edf727ba93be2e5f2818d225
PatriceARMAND <pa.armand@gmail.com>
2021-11-23 13:05:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-formtrial
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-formtrial)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./mystyle.css"; 
+import LoginForm from "./components/LoginForm";
+
+export default function App() {
+  return (
+    <div>
+      <LoginForm/>
+      
+    </div>
+  );
+}
